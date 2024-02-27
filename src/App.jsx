af34965882e05154bff7399f1b8b5dc503c6429f
CKirbs <ckggithub@gmail.com>
2024-02-27 20:25:41 +0800

    Added Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
-  return <p>nigge</p>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.svg";
+export default function Header() {
+  return (
+    <div id="header">
+      <img src={logo} alt="" />
+      <h1>20 Maths</h1>
+    </div>
+  );
+}
