6f8dfdd7c0fea260f1a7b19ab83c007d74a48be8
CKirbs <ckggithub@gmail.com>
2024-03-03 17:19:17 +0800

    Added Answer Report + Minor Tweaks

--- src/components/Finished.jsx
+++ src/components/Finished.jsx
@@ -2,8 +2,9 @@ import trophyImg from "../assets/trophy.svg";
 import SettingButton from "./SettingButton";
 export default function Finished() {
   return (
-    <div className="w-full finished-section">
+    <div className="finished-section">
       <Congratulation />
+      <AnswerReports />
     </div>
   );
 }
@@ -13,7 +14,7 @@ function Congratulation() {
     <div className="card congratulation-section">
       <h2>Congratulations!</h2>
       <p>You have successfully finished the quiz! </p>
-      <button className="card redo">Redo Quiz</button>
+      <button className="card redo">Redo Quiz</button> {/* Place this inside Answer Reports */}
       <SettingButton />
       <span>
         <img className="w-full" src={trophyImg} alt="trophy image" />
@@ -22,7 +23,30 @@ function Congratulation() {
   );
 }
 
-function AnswerReports() {}
+function AnswerReports() {
+  return (
+    <div className="card answer-report">
+      <h2>Answer Report</h2>
+      <div className="data-div">
+        <div className="card data-elem">
+          <h3>Correct</h3>
+          <p>15/20</p>
+          <p>50%</p>
+        </div>
+        <div className="card data-elem">
+          <h3>Correct</h3>
+          <p>15/20</p>
+          <p>50%</p>
+        </div>
+        <div className="card data-elem">
+          <h3>Correct</h3>
+          <p>15/20</p>
+          <p>50%</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function ReportList() {}
 
