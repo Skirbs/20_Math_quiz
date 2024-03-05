fc31e03e350741aefc7faa6a222099fb26961eff
CKirbs <ckggithub@gmail.com>
2024-03-05 18:27:21 +0800

    Answer Options Generation Functionality + minor tweaks

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -13,7 +13,6 @@ export default function Quiz() {
   return (
     <div className="card quiz-div">
       <Question digitsPerTerm={digitsPerTerm.current} operation={operation.current} />
-      <AnswerDiv />
       <QuizHeader />
     </div>
   );
@@ -30,11 +29,14 @@ function Question({digitsPerTerm, operation}) {
 
   let answer;
 
+  // if the operation is mixed (because of the settings)
+  // it will randomly choose an operation and use that for the question
   if (operation === "mixed") {
     const operationList = ["+", "-", "x", "÷"];
     operation = operationList[Math.floor(Math.random() * operationList.length)];
   }
 
+  // get the answer based on the question format
   switch (operation) {
     case "+":
       answer = first_number + second_number;
@@ -50,26 +52,30 @@ function Question({digitsPerTerm, operation}) {
     case "÷":
       const multiplied_answer = first_number * second_number;
       const original_answer = multiplied_answer / second_number;
-      first_number = multiplied_answer;
+      first_number = multiplied_answer; // for display purposes
       answer = original_answer;
       break;
   }
 
   return (
-    <div className="card question-display">
-      <h2>
-        {first_number} {operation} {second_number}
-      </h2>
-      <SettingButton />
-    </div>
+    <>
+      <div className="card question-display">
+        <h2>
+          {first_number} {operation} {second_number}
+        </h2>
+        <SettingButton />
+      </div>
+
+      <AnswerDiv correctAnswer={answer} />
+    </>
   );
 }
 
-function AnswerDiv() {
+function AnswerDiv({correctAnswer}) {
   return (
     <div className="answer-div">
       <TimerProgress />
-      <ChoicesList />
+      <ChoicesList correctAnswer={correctAnswer} />
     </div>
   );
 }
@@ -78,18 +84,48 @@ function TimerProgress() {
   return <progress value={20} max={40} />;
 }
 
-function ChoicesList() {
+function ChoicesList({correctAnswer}) {
+  let answerOptions = [correctAnswer]; // These are the options that will be display. incorrect ones will soon be added
+
+  // this for loop generates 3 altered answers
+  for (let i = 0; i < 3; i++) {
+    let alteredAnswer;
+
+    // The while loop makes sure that and option cannot be repeated again
+    while (true) {
+      // if the digits of the correct answer is less than 4 (inc. 1000) then the gap between the options will be -10 to 10
+      if (Math.abs(correctAnswer) <= 1000) {
+        alteredAnswer = correctAnswer + Math.floor(Math.random() * (10 + 9) - 10);
+      }
+
+      // if the digits of the correct answer is higher than 4 (excl. 1000) then the gap between the options will be -10*[answerDigitsCount - 2] to 10*[answerDigitsCount - 2]
+      else {
+        const answerDigitsCount = correctAnswer.toString().length;
+        const alterDifference = 10 ** (answerDigitsCount - 2);
+        alteredAnswer =
+          correctAnswer +
+          Math.floor(Math.random() * (alterDifference + alterDifference - 1) - alterDifference);
+      }
+
+      // this stops the break if the altered answer does not exist.
+      if (!answerOptions.includes(alteredAnswer)) {
+        break;
+      }
+    }
+    answerOptions.push(alteredAnswer);
+  }
+  answerOptions.sort(() => Math.random() - 0.5); // randomly shuffle the options
+
   return (
     <div className="choices">
-      <ChoiceElem />
-      <ChoiceElem />
-      <ChoiceElem />
-      <ChoiceElem />
+      {answerOptions.map((value, i) => {
+        return <ChoiceElem key={i} value={value} />;
+      })}
     </div>
   );
 }
-function ChoiceElem() {
-  return <button className="card choice-elem">a</button>;
+function ChoiceElem({value}) {
+  return <button className="card choice-elem">{value}</button>;
 }
 
 // * <- Extra Components -> * //
